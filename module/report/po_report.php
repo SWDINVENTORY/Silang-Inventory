00365eb43851126b06451ebdde65cc614f9c43ed
Garry M. Perez <perez.garrym@gmail.com>
2014-02-25 09:17:15 +0800

    delete print array

--- module/report/po_report.php
+++ module/report/po_report.php
@@ -1,7 +1,6 @@
 <?php
 include('po_report_sheet.php');
 	function recieve_data($datas){
-		//echo "<pre>";print_r($datas);exit();
 		$rc= new POReport();
 		$rc->hdr($datas);
 		$total_amount = $rc->table($datas);
