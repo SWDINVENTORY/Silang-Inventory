cabdedaea25d6e61071b9b5900c6c1eb7d83eb14
cdabanes <cdabanes@gmail.com>
2015-07-25 15:54:52 +0800

    purpose

--- front/page/report.php
+++ front/page/report.php
@@ -58,6 +58,9 @@ class Front_Page_Report extends Front_Page {
             case 'monthly-report' :
                 return $this->report_MONTHLY();
                 break;
+			case 'monthly-receiving-report' :
+                return $this->report_MONTHLY_RECEIVING();
+                break;
             case 'ris' :
                 if (isset($_GET['ris_no'])) {
                     $data = $this->Requisition()->getByRisNo($_GET['ris_no']);
@@ -198,6 +201,7 @@ class Front_Page_Report extends Front_Page {
             ->setColumns(array('issuance.created',
                 'ris_no',
                 'ris_division',
+                'ris_purpose',
                 'ris_dtl_item_desc',                
                 'ris_dtl_item_size',
                 'issuance_dtl_item_charging',
@@ -230,7 +234,7 @@ class Front_Page_Report extends Front_Page {
                 $issued_item[$index] = array(
                     'date' => date('m/d/Y', strtotime($issued[$i]['created'])),
                     'ris_no' => $issued[$i]['ris_no'],
-                    'rc_desc' => $issued[$i]['ris_division'],
+                    'rc_desc' => $issued[$i]['ris_purpose'],
                     'inv_desc' => $issued[$i]['ris_dtl_item_desc'],
                     'issued_qty' => $issued[$i]['issuance_dtl_item_issued'],
                     'unit_cost' => $issued[$i]['ris_dtl_item_cost'],
@@ -384,6 +388,11 @@ class Front_Page_Report extends Front_Page {
         $rc->output();
     }
 
+	protected function report_MONTHLY_RECEIVING(){
+		echo "DITO GARRY :)!";
+		exit;
+	}
+	
     protected function report_PHYSICAL_COUNT() {
 		$from  = date('Y-m-d 0:0:0', strtotime($this->get['from_month']));
 		$to  = date('Y-m-d 13:59:59', strtotime($this->get['to_month']));
